a5ddb8b0f917c2fe914b1ae64d9db5077766e2c4
McNeil <student3011@ad.uconn.edu>
2018-07-10 09:33:33 -0400

    Create CSS for our design, fix emailer bug

--- pages/index.vue
+++ pages/index.vue
@@ -70,12 +70,13 @@
 
     </div>
 
-    <button class="submit" v-on:click="submit">{{submitText}}</button>
+    <button class="submit" v-bind:class="{disabled:submitText!=='Submit'}" v-on:click="submit">{{submitText}}</button>
 
   </div>
 </template>
 
 <script>
+
 import checkBox from '~/components/checkBox.vue'
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
@@ -86,7 +87,7 @@ export default {
     checkBox
   },
   data: () => ({
-    account: { //Not using camel case because user may see these keys
+    account: { //Not using camel case because user will see these keys (IE: "Error, CostEstimate not entered!")
       KFS:'',
       Name:'',
       Department:'',
@@ -112,38 +113,44 @@ export default {
   }),
   methods: {
     copyContact(){
-      this.contact.Name=this.account.Name
-      this.contact.Phone=this.account.Phone
-      this.contact.Email=this.account.Email
+      this.contact.Name = this.account.Name
+      this.contact.Phone = this.account.Phone
+      this.contact.Email = this.account.Email
     },
     submit(){
+      fetch('/api/submit',{
+        method: 'post',
+        headers: {"Content-Type": "application/json; charset=utf-8",},
+        body: JSON.stringify(this.$data)
+      })
+      .then(function(response) {
+        if (response.status >= 400) {
+          console.error("Bad response from server");
+          console.log(response)
+        }
+      })
 
-
-fetch('/api/submit',{
-    method: 'post',
-    headers: {"Content-Type": "application/json; charset=utf-8",},
-    body: JSON.stringify(this.$data)
-  })
-	.then(function(response) {
-		if (response.status >= 400) {
-			throw new Error("Bad response from server");
-		}
-		return response.json();
-	})
-	.then(function(stories) {
-		console.log(stories);
-	});
     }
   },
   computed: {
     submitText(){
-      return verifyForm(this.$data)
+      let errorMsg = verifyForm(this.$data)
+      if(errorMsg===0) return 'Submit'
+      return errorMsg
     }
   }
 }
 </script>
 
 <style>
+
+@import url('https://fonts.googleapis.com/css?family=Space+Mono');
+
+html, body {
+  font-family: "Space Mono";
+  background: #e2e1e0;
+}
+
 input {
   font-size: 1em;
 }
@@ -151,47 +158,112 @@ input {
 .header {
   text-align: center;
 }
+
 .header h1 {
   margin-bottom: 1em;
 }
+
 .form{
   display: flex;
   flex-direction: column;
   margin: 2em;
 }
+
 .area {
   margin: 2em;
-  border: solid black 1px;
+  border-radius: 1px;
   padding: 1em;
+  background-color: white;
+  box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
 }
+
 .area h2 {
   margin-bottom: 1em;
 }
+
 .oneLiner {
   display: flex;
   margin-top: 1em;
   flex-wrap: wrap;
 }
+
 .oneLiner * {
   margin-top: 1em;
   flex: 1 1 0;
 }
+
 .oneLiner h3{
   margin-right: 2em;
 }
+
 .oneLiner input {
   margin-right: 1em; 
 }
+
 #addressBox {
   flex-grow: 1;
 }
+
+.textBox {
+  border: none;
+  outline: none;
+  background-color: rgb(226, 226, 226);
+  padding: 0.5em;
+  border-radius: 2px;
+  transition: ease 0.3s all;
+}
+
+.textBox:hover {
+  background-color: rgb(212, 212, 212);
+}
+
+.textBox:focus {
+  background-color: rgb(145, 196, 209);
+}
+
+hr {
+  color: #BDBDBD;
+}
+
 .description {
   width: 100%;
   margin-top: 1em;
 }
+
+.copyContact {
+  font-size: 1.1em;
+  border: none;
+  outline: none;
+  padding: 1em;
+  border-radius: 2px;
+  transition: ease 0.3s all;
+}
+.copyContact:hover {
+  background-color: rgb(142, 210, 235);
+  cursor: pointer;
+}
+
 .submit {
   margin: 1em 8em 0 8em;
   font-size: 1.5em;
+  border: none;
+  outline: none;
+  padding: 1em;
+  border-radius: 2px;
+  transition: ease 0.3s all;
+  box-shadow: 0 1px 3px rgba(0,0,0,0.16), 0 1px 3px rgba(0,0,0,0.23);
+  background-color: rgb(214, 255, 214);
+}
+
+.submit:hover {
+  box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+  cursor: pointer;
+  background-color: rgb(153, 255, 153);
+}
+
+.submit.disabled{
+  pointer-events: none;
+  background-color:rgb(255, 184, 184);
 }
 
 </style>
